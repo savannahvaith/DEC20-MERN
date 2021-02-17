7994a652aff565e53528bb36105a1d31cbca9826
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-17 14:10:40 +0000

    SV: Changed app.listen()

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
     res.send("hello world!");
 });
 
+app.get("/hello", (req, res) => {
+    res.status(200).send("This is the hello route");
+    // console.log(`heyyyy!`);
+});
+
+app.get("/error", (req,res)=>{
+    res.status(500).send("oops");
+});
 
 // communicate with the app on a specific port
-app.listen(5019);
+const server = app.listen(5019, () => {
+    console.log(`Server has successfully started on port number: ${server.address().port}`);
+})
