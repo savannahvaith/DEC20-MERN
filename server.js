d3d3167dc53b0a1c1e28d82ce6d06fca0ebd24af
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-17 15:24:01 +0000

    SV: Added custom middleware

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const bodyParser = require('body-parser');
 
 // instantiate my app 
 const app = express(); 
+// middleware!
 app.use(cors());
 app.use(bodyParser.json());
+
+//Custom middleware
+const logger = (req, res, next) => {
+    console.log(new Date());
+    next();
+}
+
+// this will print the date before each request.. 
+app.use(logger);
 
 // importing all of the routes in product.js
 const productRoute = require('./routes/products');
