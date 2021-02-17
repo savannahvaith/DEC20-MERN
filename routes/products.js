84d759d7686b16268132dd5bd15ff52832ace597
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-17 15:19:57 +0000

    SV: Added req breakdown

--- routes/products.js
+++ routes/products.js
@@ -1,3 +1,4 @@
+'use strict';
 const router = require('express').Router();
 
 // requests (C,R,U,D)
@@ -15,5 +16,26 @@ router.post("/create", (req,res,next) => {
     const name = req.body.name; 
     res.send(`Hello there ${name}`);
 });
+
+// url parameters?
+router.delete("/delete/:id", (req,res,next) => {
+    console.log(req.params.id);
+    res.send(`done`);
+});
+
+// query parameter
+router.patch("/update/:id", (req,res,next) => {
+    const id = req.params.id; 
+    console.log(`id: ${id}`);
+    // const name = req.query.name; 
+    // const age = req.query.age;
+
+    // destructuring?
+    const {name, age} = req.query; 
+    console.log(`name: ${name}, age: ${age}`);
+    res.send(`got your info, k thanks bye`);
+
+});
+
 
 module.exports = router; 
