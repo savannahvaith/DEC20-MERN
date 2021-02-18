dc10df24304e856351a1a46a71a6c3ae6afcbda8
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-18 10:33:31 +0000

    SV: Added HTTP-Error module and error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const createError = require('http-errors');
 
 // instantiate my app 
 const app = express(); 
@@ -22,6 +23,15 @@ app.use(logger);
 const productRoute = require('./routes/products');
 
 app.use("/product",productRoute);
+
+// error handling
+app.use((req,res,next) => {
+    next(createError(404, 'Resource not found'));
+});
+
+app.use((err,req,res,next)=>{
+    res.status(err.statusCode || 500).send(err.message || "something went wrong");
+})
 
 // communicate with the app on a specific port
 const server = app.listen(5019, () => {
