f6234b040a2827ca4952e62280a62158203f1737
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-22 12:13:15 +0000

    SV: Changed App into functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
-function App() {
+'use strict';
+
+import Standard from "./Components/02-Parents-Child/Standard";
+import Introduction from "./Components/01-Intro/First";
+import Second from "./Components/01-Intro/Second";
+
+const App = () => {
   return (
     <div className="App">
       <p>Sav says hi </p>
+      {/* <Introduction/> */}
+      {/* <Second/>*/}
+      <Standard/> 
     </div>
   );
 }
