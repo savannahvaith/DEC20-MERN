1683e9b60bf11d64297fef09b71ca96734ecfc83
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-23 09:51:16 +0000

    SV: Added Prop Types Example

--- frontend/src/Components/03-Props/Static-Data/Post.jsx
+++ frontend/src/Components/03-Props/Static-Data/Post.jsx
@@ -1,15 +1,26 @@
 'use strict';
+import PropTypes from 'prop-types';
 
 // const Post = ({name,mail}) => {
 const Post = (props) => {
-    console.log(props);
+
     const { name, mail } = props;
 
     return (
         <>
+            <h3>{props.headerText}</h3>
             <p>{name}</p>
             <a href="mailto:svaithilingam@qa.com">{mail}</a>
         </>
     );
 }
+Post.defaultProps = {
+    headerText: `Use this value, if nothing is passed in!`
+}
+
+Post.propTypes = { 
+    name: PropTypes.string.isRequired,
+    mail: PropTypes.string
+}
+
 export default Post; 
