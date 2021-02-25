3b4ab613b180d1a30008e9f15aadbe840fcfc78e
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-25 11:00:04 +0000

    SV: Added message and container

--- frontend/src/Components/Header/HomePage.jsx
+++ frontend/src/Components/Header/HomePage.jsx
@@ -1,13 +1,22 @@
 import CreateGoal from "../SideBar/CreateGoal"
+import {useState} from 'react'; 
 
 const HomePage = () => {
+
+    const [msg, setMsg] = useState('');
+    
+    const trigger = (data) => {
+        setMsg(data);
+    }
 
     return (
         <div className="row">
             <div className="col-md-2">
-                <CreateGoal />
+                <CreateGoal trigger={trigger}/>
             </div>
-
+            <div className="col-md-10">
+                <div className="alert alert-success">{msg}</div>
+            </div>
         </div>
     )
 }
