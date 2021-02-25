db5ccd2693c25e6278b2f2b36e7848a063594f36
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-25 12:41:46 +0000

    SV: Changed Card to Toast

--- frontend/src/Components/IndividualGoal.jsx
+++ frontend/src/Components/IndividualGoal.jsx
@@ -1,20 +1,25 @@
-import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import { Card, CardBody, CardSubtitle, CardTitle, Toast, ToastBody, ToastHeader } from "reactstrap";
 import DeleteGoal from "./DeleteGoal";
 import EditGoal from "./EditGoal";
 
-const IndividualGoal = ({ item , trigger}) => {
+const IndividualGoal = ({ item, trigger }) => {
 
     return (
-        <Card>
-            <CardBody>
-                <CardTitle tag="h3">{item.title}</CardTitle>
-                <CardSubtitle className="mb-2 text-muted">{item.description}</CardSubtitle>
-                <div className="float-right">
-                    <DeleteGoal del={item._id} trigger={trigger}/>
-                    <EditGoal item={item} trigger={trigger}/>
-                </div>
-            </CardBody>
-        </Card>
+        // <Card>
+        <div className="p-6 my-2 rounded">
+            <Toast>
+                <ToastHeader>{item.title}
+                </ToastHeader>
+                <ToastBody style={{ height: "auto" }}>
+                    {item.description}
+                    <hr />
+                    <DeleteGoal className="float-right" del={item._id} trigger={trigger} />
+                    <EditGoal className="float-right" item={item} trigger={trigger} />
+
+                </ToastBody>
+            </Toast>
+        </div>
+
     )
 }
 export default IndividualGoal; 
