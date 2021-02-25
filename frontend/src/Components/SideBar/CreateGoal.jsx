9522abe5f2168ffab18054a80a5ac544fce484c1
savannahvaith <sav.vaithilingam@gmail.com>
2021-02-25 10:09:45 +0000

    SV: Minimum Template for Create

---
+++ frontend/src/Components/SideBar/CreateGoal.jsx
@@ -0,0 +1,16 @@
+import { Card, CardBody, CardTitle } from "reactstrap"
+
+const CreateGoal = () => {
+
+    return(
+        <div className="bg-light border-right" id="sidebar">
+            <Card>
+                <CardBody>
+                    <CardTitle>Create New List</CardTitle>
+                </CardBody>
+            </Card>
+        </div>     
+    )
+}
+
+export default CreateGoal; 
